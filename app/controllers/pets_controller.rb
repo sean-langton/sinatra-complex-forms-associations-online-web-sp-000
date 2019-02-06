12545fb264078002217c96d603e5a678d370287c
Sean Langton <slangton1@gmail.com>
2019-02-06 20:03:34 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,7 +16,7 @@ class PetsController < ApplicationController
       @owner = Owner.create(name: params[:owner_name])
     else
       puts params[:owner][:id]
-      @owner = Owner.find(params[:owner][:id])
+      @owner = Owner.find(params[:owner][:id][])
     end
       @owner.pets << @pet
     redirect to "pets/#{@pet.id}"
