ab5a29b919114571b9c7c31108b02a01c2e87de6
Sean Langton <slangton1@gmail.com>
2019-02-06 20:02:24 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,10 +14,10 @@ class PetsController < ApplicationController
     @pet = Pet.create(name: params[:pet_name])
     if !params[:owner_name].empty?
       @owner = Owner.create(name: params[:owner_name])
-      @owner.pets << @pet
     else
       @owner = Owner.find(params[:owner][:id])
     end
+      @owner.pets << @pet
 
     redirect to "pets/#{@pet.id}"
   end
